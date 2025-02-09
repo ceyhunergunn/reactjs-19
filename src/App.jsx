ed95544ddb4906776f6389721349c01970b5ca5a
ceyhunergunn <ceyhunergun61@gmail.com>
2025-02-09 19:19:26 +0300

    add assets and header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 
-//eslint-disable-next-line
 // const Card = ({ title }) => {
 //   const [count, setCount] = useState(0);
 //   const [hasLiked, setHasLiked] = useState(false);
@@ -39,7 +38,10 @@ import { useState, useEffect } from "react";
 //   );
 // };
 
+import Search from "./components/Search";
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     // <div className="card-container">
     //   <Card
@@ -52,7 +54,20 @@ const App = () => {
     //   <Card title="The Lion King" />
     // </div>
 
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   );
 };
 
