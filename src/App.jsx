c8c94f39a5cf67c38d185393d91a991737574558
ceyhunergunn <ceyhunergun61@gmail.com>
2025-02-09 18:04:56 +0300

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 const Card = ({ title }) => {
   return (
-    <div>
+    <div
+      className="card"
+      // inline style
+      // style={{
+      //   border: "1px solid #4b5462",
+      //   padding: "20px",
+      //   margin: "10px",
+      //   backgroundColor: "#31363f",
+      //   borderRadius: "10px",
+      //   minHeight: "100px",
+      // }}
+    >
       <h2>{title}</h2>
     </div>
   );
@@ -8,9 +19,7 @@ const Card = ({ title }) => {
 
 const App = () => {
   return (
-    <div>
-      <h2>Functional Arrow Component</h2>
-
+    <div className="card-container">
       <Card
         title="Star Wars"
         rating={5}
