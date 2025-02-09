0a14f7103b3e52347d46251922b637aa8b987a13
ceyhunergunn <ceyhunergun61@gmail.com>
2025-02-09 18:48:45 +0300

    import tailwindcss and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,58 @@
 import { useState, useEffect } from "react";
 
 //eslint-disable-next-line
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
+// const Card = ({ title }) => {
+//   const [count, setCount] = useState(0);
+//   const [hasLiked, setHasLiked] = useState(false);
 
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-    //eslint-disable-next-line
-  }, [hasLiked]);
+//   useEffect(() => {
+//     console.log(`${title} has been liked: ${hasLiked}`);
+//     //eslint-disable-next-line
+//   }, [hasLiked]);
 
-  useEffect(() => {
-    console.log("Card Rendered");
-  }, []);
-  return (
-    <div
-      className="card"
-      onClick={() => setCount(count + 1)}
-      // inline style
-      // style={{
-      //   border: "1px solid #4b5462",
-      //   padding: "20px",
-      //   margin: "10px",
-      //   backgroundColor: "#31363f",
-      //   borderRadius: "10px",
-      //   minHeight: "100px",
-      // }}
-    >
-      <h2>
-        {title}
-        <br />
-        {count || null}
-      </h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+//   useEffect(() => {
+//     console.log("Card Rendered");
+//   }, []);
+//   return (
+//     <div
+//       className="card"
+//       onClick={() => setCount(count + 1)}
+//       // inline style
+//       // style={{
+//       //   border: "1px solid #4b5462",
+//       //   padding: "20px",
+//       //   margin: "10px",
+//       //   backgroundColor: "#31363f",
+//       //   borderRadius: "10px",
+//       //   minHeight: "100px",
+//       // }}
+//     >
+//       <h2>
+//         {title}
+//         <br />
+//         {count || null}
+//       </h2>
+//       <button onClick={() => setHasLiked(!hasLiked)}>
+//         {hasLiked ? "❤️" : "🤍"}
+//       </button>
+//     </div>
+//   );
+// };
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card
-        title="Star Wars"
-        rating={5}
-        isCool={true}
-        actors={[{ name: "Ceyhun", surname: "Ergün" }]}
-      />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
+    // <div className="card-container">
+    //   <Card
+    //     title="Star Wars"
+    //     rating={5}
+    //     isCool={true}
+    //     actors={[{ name: "Ceyhun", surname: "Ergün" }]}
+    //   />
+    //   <Card title="Avatar" />
+    //   <Card title="The Lion King" />
+    // </div>
+
+    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
   );
 };
 
